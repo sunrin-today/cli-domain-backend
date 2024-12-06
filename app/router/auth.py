7245f40d3c2c640df4e8f10363622650807846e8
Yuchan Han <serltretu24@gmail.com>
2024-12-07 03:07:48 +0900

    auth router (get authorization url)

--- app/router/auth.py
+++ app/router/auth.py
@@ -1,22 +1,55 @@
-from fastapi import APIRouter
+import aiogoogle.excs
+from dependency_injector.wiring import Provide, inject
+from fastapi import APIRouter, Query, Depends, status
 from fastapi_restful.cbv import cbv
 
-from app.core.response import APIResponse
+from app.core.error import ErrorCode
+from app.core.response import APIResponse, APIError
+from app.service.container import ServiceContainer
+from app.service.google import GoogleRequestService
 
 router = APIRouter(
-    prefix="/hello",
-    tags=["Hello"],
+    prefix="/auth",
+    tags=["Authorization"],
     responses={404: {"description": "Not found"}},
 )
 
 
 @cbv(router)
-class Hello:
+class AuthController:
 
-    @router.get("/world")
-    async def get_home(self) -> APIResponse[dict]:
+    @router.get("/authorization-url")
+    @inject
+    async def redirect_to_authorization_url(
+        self,
+        session_id: str = Query(...),
+        google_service: GoogleRequestService = Depends(
+            Provide[ServiceContainer.google]
+        ),
+    ) -> APIResponse[dict]:
+        url = await google_service.get_authorization_url(session_id)
         return APIResponse(
-            message="Hello, World!",
-            data={"message": "Hello, World!"},
+            data={"authorization_url": url}, message="Authorization URL generated"
         )
 
+    @router.get("/callback")
+    @inject
+    async def callback(
+        self,
+        code: str = Query(...),
+        state: str = Query(...),
+        google_service: GoogleRequestService = Depends(
+            Provide[ServiceContainer.google]
+        ),
+    ) -> APIResponse[dict]:
+        try:
+            credentials = await google_service.fetch_user_credentials(code)
+            user_data = await google_service.fetch_user_info(credentials)
+        except aiogoogle.excs.HTTPError as e:
+            raise APIError(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                error_code=ErrorCode.INVALID_GOOGLE_CREDENTIALS,
+                message="Google API Error",
+                error_data={"error": str(e)},
+            )
+        return APIResponse(data=user_data, message="User credentials fetched")
