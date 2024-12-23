03340f14e9535b836c60bea4e3798a97cb3a69b7
Yuchan Han <serltretu24@gmail.com>
2024-12-23 13:26:59 +0900

    user entity에 프로필 이미지 추가

--- app/entity/user.py
+++ app/entity/user.py
@@ -5,6 +5,7 @@ class User(Model):
     id = fields.UUIDField(pk=True)
     nickname = fields.CharField(max_length=50)  # google.user_data["name"]
     email = fields.CharField(max_length=100)  # google.user_data["email"]
+    avatar = fields.CharField(max_length=200)  # google.user_data["avatar"]
     limit = fields.IntField(default=5)  # google.user_data["limit"]
     tickets = fields.ManyToManyField("models.DomainTicket", related_name="users")
     domains = fields.ManyToManyField("models.Domain", related_name="users")
