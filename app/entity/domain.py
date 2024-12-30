5bebadb9f398bea0662a3028a149e50548db8ab5
Yuchan Han <serltretu24@gmail.com>
2024-12-30 11:33:23 +0900

    도메인에 cloudflare record id 추가

--- app/entity/domain.py
+++ app/entity/domain.py
@@ -7,6 +7,7 @@ class Domain(Model):
     id = fields.UUIDField(pk=True)
     name = fields.CharField(max_length=100)
     user: fields.ForeignKeyRelation["User"]
-    created_at = fields.DatetimeField(auto_now_add=True)
-    updated_at = fields.DatetimeField(auto_now=True)
+    created_at = fields.DatetimeField(auto_now=True)
+    updated_at = fields.DatetimeField(auto_now_add=True)
+    record_id = fields.CharField(max_length=100, null=True)
     log = fields.ManyToManyField("models.DomainLog", related_name="domains")
