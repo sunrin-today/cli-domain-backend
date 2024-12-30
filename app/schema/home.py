00a1e4868d17d5d34f884d5707975e38f7f87456
Yuchan Han <serltretu24@gmail.com>
2024-12-30 11:35:46 +0900

    도메인 Transfer router용 DTO 모델 추가

--- app/schema/home.py
+++ app/schema/home.py
@@ -1,6 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+
+from app.core.pydantic_model import BaseSchema
 
 
 class HomeSchema(BaseModel):
     message: str
     data: dict[str, str] | None
+
+
+class TransferDomainDTO(BaseSchema):
+    name: str
+    user_email: EmailStr
