557925a33b38d95140058fbcf4bb1e66fd532944
Yuchan Han <serltretu24@gmail.com>
2025-01-03 08:38:07 +0900

    auth 인증시 jinja template 반환으로 변경

--- app/router/auth.py
+++ app/router/auth.py
@@ -2,14 +2,22 @@ import uuid
 
 import aiogoogle.excs
 from dependency_injector.wiring import Provide, inject
-from fastapi import APIRouter, Query, Depends, status, Request
+from fastapi import APIRouter, Query, Depends, status, Request, BackgroundTasks
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from fastapi_restful.cbv import cbv
 from starlette.responses import RedirectResponse, HTMLResponse
+from starlette.websockets import WebSocketDisconnect
 from slowapi import Limiter
 from slowapi.util import get_remote_address
 from starlette.websockets import WebSocket
 
-from app.core.deps import get_current_user_entity
+from app.core.deps import get_current_user_entity, get_user_token
+from app.core.string import (
+    parse_application_url,
+    create_application_redirect_url,
+    create_application_reject_url,
+)
 from app.entity import User
 from app.core.error import ErrorCode
 from app.core.response import APIResponse, APIError
@@ -30,6 +38,7 @@ limiter = Limiter(
     key_func=get_remote_address,
     storage_uri=settings.REDIS_URI,
 )
+templates = Jinja2Templates(directory="template")
 _log = use_logger("auth-controller")
 
 
@@ -48,10 +57,26 @@ class AuthController:
         url = await google_service.get_authorization_url(session_id)
         return RedirectResponse(url=url)
 
+    @router.get("/app/rejected")
+    async def app_rejected(
+        self, request: Request, name: str = Query(...)
+    ) -> HTMLResponse:
+        return templates.TemplateResponse(
+            request=request,
+            name="login.html",
+            context={
+                "status": "failed",
+                "message": "애플리케이션 요청이 거절됨.",
+                "detail": f"사용자가 {name} 애플리케이션 요청을 거절했습니다.",
+                "login_data": f"AppName: {name}",
+            },
+        )
+
     @router.get("/callback")
     @inject
     async def callback(
         self,
+        request: Request,
         code: str = Query(...),
         session_id: str = Query(alias="state"),
         google_service: GoogleRequestService = Depends(
@@ -71,11 +96,15 @@ class AuthController:
             user_data = await google_service.fetch_user_info(credentials)
         except aiogoogle.excs.HTTPError as e:
             _log.error(f"Google API Error: {e.res}")
-            return HTMLResponse(
-                content=f"<h1>Error!</h1>"
-                f"<p>구글 로그인 오류입니다</p>"
-                f"<p>ErrorData: {e.res}</p>"
-                f"<p>session id: {session_id}</p>",
+            return templates.TemplateResponse(
+                request=request,
+                name="login.html",
+                context={
+                    "status": "failed",
+                    "message": f"구글 로그인 오류입니다.",
+                    "detail": str(e.res),
+                    "login_data": session_id,
+                },
             )
 
         if not await User.filter(email=user_data["email"]).exists():
@@ -97,11 +126,88 @@ class AuthController:
         else:
             user_entity = await User.filter(email=user_data["email"]).first()
 
+        if session_id.startswith("@"):
+            parsed_data = parse_application_url(session_id)
+            if not parsed_data["application"] in ["vercel", "transfer"]:
+                return templates.TemplateResponse(
+                    request=request,
+                    name="login.html",
+                    context={
+                        "status": "failed",
+                        "message": f"애플리케이션 로그인 오류",
+                        "detail": "잘못된 application url입니다.",
+                        "login_data": session_id,
+                    },
+                )
+            new_access_token = await user_session.create_new_token(str(user_entity.id))
+            temporary_application_only_id = (
+                "App" + parsed_data["application"].capitalize() + str(uuid.uuid4())
+            )
+            await login_service.push_token_to_session(
+                temporary_application_only_id, new_access_token
+            )
+            parsed_data["parameters"].update({"token": new_access_token})
+            redirect_url = create_application_redirect_url(
+                base_url=settings.BACKEND_HOST,
+                application=parsed_data["application"],
+                route=parsed_data["route"],
+                params=parsed_data["parameters"],
+            )
+            application_permission = {
+                "vercel": [
+                    {
+                        "icon": "fas fa-user",
+                        "name": "프로필 정보 읽기",
+                        "content": "이름, 이메일, 프로필 사진을 읽습니다.",
+                    },
+                    {
+                        "icon": "fa-solid fa-tower-cell",
+                        "name": "도메인 관리",
+                        "content": "자동으로 도메인을 연결합니다.",
+                    },
+                    {
+                        "icon": "fa-solid fa-circle-nodes",
+                        "name": "Vercel 계정으로 로그인",
+                        "content": "Vercel 계정에 접근하여 도메인을 자동으로 인증받습니다.",
+                    },
+                ],
+                "transfer": [
+                    {
+                        "icon": "fas fa-user",
+                        "name": "프로필 정보 읽기",
+                        "content": "도메인 이전 대상 사용자가 맞는지 확인합니다.",
+                    },
+                    {
+                        "icon": "fa-solid fa-tower-cell",
+                        "name": "도메인 관리",
+                        "content": "다른 유저의 도메인을 내 계정으로 이전합니다.",
+                    },
+                ],
+            }
+            return templates.TemplateResponse(
+                request=request,
+                name="app-login.html",
+                context={
+                    "name": parsed_data["application"].capitalize(),
+                    "permissions": application_permission[parsed_data["application"]],
+                    "accept_url": redirect_url,
+                    "reject_url": create_application_reject_url(
+                        base_url=settings.BACKEND_HOST,
+                        app_name=parsed_data["application"],
+                    ),
+                },
+            )
+
         if not await login_service.exist_session(session_id=session_id):
-            return HTMLResponse(
-                content=f"<h1>Error!</h1>"
-                f"<p>로그인 세션을 찾을 수 없습니다</p>"
-                f"<p>session id: {session_id}</p>",
+            return templates.TemplateResponse(
+                request=request,
+                name="login.html",
+                context={
+                    "status": "failed",
+                    "message": f"알 수 없는 세션",
+                    "detail": "로그인 세션을 찾을 수 없습니다.",
+                    "login_data": session_id,
+                },
             )
 
         await login_service.set_session_user(
@@ -112,12 +218,15 @@ class AuthController:
             new_access_token = await user_session.create_new_token(str(user_entity.id))
             await login_service.push_token_to_session(session_id, new_access_token)
             await login_service.delete_session(session_id)
-
-        return HTMLResponse(
-            content=f"<h1>Success!</h1>"
-            f"<p>로그인에 성공했습니다</p>"
-            "<p>콘솔로 다시 돌아가세요</p>"
-            f"<p>session id: {session_id}</p>",
+        return templates.TemplateResponse(
+            request=request,
+            name="login.html",
+            context={
+                "status": "success",
+                "message": f"로그인에 성공했습니다",
+                "detail": "콘솔로 다시 돌아가세요",
+                "login_data": session_id,
+            },
         )
 
     @router.get("/login/session")
@@ -182,14 +291,15 @@ class AuthController:
         )
 
     @router.post("/logout")
+    @inject
     async def logout(
         self,
-        user: User = Depends(get_current_user_entity),
+        token: str = Depends(get_user_token),
         user_session: UserSessionService = Depends(
             Provide[ServiceContainer.user_session]
         ),
     ) -> APIResponse[dict]:
-        await user_session.delete_token(str(user.id))
+        await user_session.delete_token(token)
         return APIResponse(message="Logout success")
 
 
@@ -210,5 +320,8 @@ async def wait_for_auth(
         )
     await websocket.send_json({"message": "Waiting for authentication"})
     await login_service.subscribe_session(session_id, websocket)
-    while True:
-        _ = await websocket.receive_bytes()
+    try:
+        while True:
+            _ = await websocket.receive_bytes()
+    except WebSocketDisconnect:
+        await websocket.close()
